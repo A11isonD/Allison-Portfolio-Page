13104568d1f83f16cae9a51db1ee8ef1a17e8f3b
Allison Dobbs <104958198+A11isonD@users.noreply.github.com>
2022-07-22 20:08:12 -0400

    changed things

--- home.js
+++ home.js
@@ -1,2 +1,12 @@
-alert("hello world");
-alert("Second Message");
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click"), () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+} ))
